1b2405fe333a5245ee55aa79e7c6efd443ef9ba0
Napollo-Software <62165761+emazeem@users.noreply.github.com>
2023-09-02 21:40:17 +0500

    ds

--- app/Models/TaskAsset.php
+++ app/Models/TaskAsset.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class TaskAsset extends Model
 {
     use HasFactory;
-    public function getCreatedAtAttribute()
+    public function getImagesAttribute()
     {
         return url('storage/tasks/'.$this->image);
     }
