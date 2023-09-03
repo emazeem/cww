9cfd828eb18654689637e08223c902f93e657d3c
Napollo-Software <62165761+emazeem@users.noreply.github.com>
2023-09-04 01:21:35 +0500

    Update IndexController.php

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -184,14 +184,12 @@ class IndexController extends Controller
         $cars=Car::with('order','order.tasks','order.tasks.assets','order.subscription')->where('user_id',$request->id)->get();
 
         foreach ($cars as $car){
-            foreach ($car->order as $order){
-                foreach ($order->tasks as $task){
-                    $images=[];
-                    foreach ($task->assets as $asset){
-                        $images[]=$asset->image;
-                    }
-                    $task->images=$images;
+            foreach ($car->order->tasks as $task){
+                $images=[];
+                foreach ($task->assets as $asset){
+                    $images[]=$asset->image;
                 }
+                $task->images=$images;
             }
         }
         return $this->sendSuccess("Cars fetched successful",$cars);
