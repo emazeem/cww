eed5dc731e4f8434e8c76f77eb6746f0d5be38d6
Napollo-Software <62165761+emazeem@users.noreply.github.com>
2023-10-10 02:29:43 +0500

    ds

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -464,6 +464,8 @@ class IndexController extends Controller
             logActivity(auth()->user()->name.' has created new order have one time wash for '.$customer->name);
         }
         $this->bookInvoice($order);
+        $user=User::find($request->user_id);
+        one_signal_notification($user->id,"{$user->name} you have a car wash today",['url'=>'task','id'=>$task->id]);
 
         return $this->sendSuccess("Car and subscription created successfully", $car);
     }
