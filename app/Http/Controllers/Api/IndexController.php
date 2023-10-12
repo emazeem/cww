75054d51177dc82967a9c66f5ab6762ab3aad8a9
Napollo-Software <62165761+emazeem@users.noreply.github.com>
2023-10-13 02:20:01 +0500

    Update IndexController.php

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -447,7 +447,7 @@ class IndexController extends Controller
         }
     }
     public function createCarSubscription(Request $request){
-        dd($request->inside,$request->outside);
+        dd(explode('@',$request->inside));
         $validators = Validator($request->all(), [
             'make' => 'required',
             'model' => 'required',
